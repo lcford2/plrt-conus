52d1110a88046d2fe17eb828512ff756255fbe30
Lucas Ford <lcford2@ncsu.edu>
2022-12-23 17:41:20 -0500

    handle multiple IP results for rsync script

--- shell_scripts/rsync_data_dirs.sh
+++ shell_scripts/rsync_data_dirs.sh
@@ -3,6 +3,10 @@
 # find ssh client
 ssh_sessions="$(sudo lsof -i TCP -s tcp:established -n | grep 'ssh->' | grep $USER)"
 ip="$(echo $ssh_sessions | grep -oP '>(\d{1,3}\.){3}\d{1,3}')"
+
+ip_array=($ip)
+ip="${ip_array[0]}"
+
 # trim off arrow
 ip=${ip:1}
 
@@ -13,6 +17,7 @@ else
 fi
 
 remotehost="$username@$ip"
-# rsync directories
+rsync directories
+echo "RSYNCing with $remotehost"
 rsync -azP ../data/ $remotehost:~/projects/plrt-conus/data
 rsync -azP ../results/ $remotehost:~/projects/plrt-conus/results
