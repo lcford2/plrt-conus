b22e8f1a375af743a97634a015da084b3fddab7e
Lucas Ford <lcford2@ncsu.edu>
2022-12-23 17:40:50 -0500

    results io functions

--- python_scripts/utils/io.py
+++ python_scripts/utils/io.py
@@ -1,3 +1,6 @@
+import pathlib
+import pickle
+
 import pandas as pd
 
 
@@ -10,3 +13,18 @@ def load_feather(file, index_keys=()):
 
 def write_feather(df, file):
     df.reset_index().to_feather(file)
+
+
+def load_pickle(file):
+    with open(file, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+def write_pickle(data, file):
+    with open(file, "wb") as f:
+        pickle.dump(data, f, protocol=4)
+
+
+def load_results(path):
+    return load_pickle((pathlib.Path(path) / "results.pickle").as_posix())
