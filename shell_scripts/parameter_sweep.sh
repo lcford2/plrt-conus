f14d32f20bd120680d186aaa010015be1765b1a8
Lucas Ford <lcford2@ncsu.edu>
2023-01-26 13:15:45 -0500

    parameter sweep for updated model

--- shell_scripts/parameter_sweep.sh
+++ shell_scripts/parameter_sweep.sh
@@ -2,8 +2,8 @@
 
 cd ../python_scripts
 
-for depth in $(seq 1 10); do
+for depth in $(seq 1 6); do
     for mss in $(seq -f "%02g" 1 10); do
-        pipenv run python fit_plrt_model.py -d $depth --mss "0.$mss"
+        pipenv run python fit_plrt_model.py -d $depth --mss "0.$mss" --min-years 3 -m -p
     done
 done
