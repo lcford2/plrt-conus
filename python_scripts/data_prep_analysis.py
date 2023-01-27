ca963610bd78e62e2eb571b17a306855a7459123
Lucas Ford <lcford2@ncsu.edu>
2023-01-27 10:24:10 -0500

    data prep analysis updates

--- python_scripts/data_prep_analysis.py
+++ python_scripts/data_prep_analysis.py
@@ -1,8 +1,9 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from fit_plrt_model import load_resopsus_data
 from palettable.cartocolors.qualitative import Vivid_5
-from parameter_sweep_analysis import get_contiguous_wbds, setup_map
+from parameter_sweep_analysis import setup_wbd_map
 from utils.config import config
 from utils.io import load_feather, load_pickle
 
@@ -40,21 +41,7 @@ def determine_huc2_trimming_changes(noncon=False):
 
 
 def plot_trimming_changes_map(noncon=False):
-    fig, ax = plt.subplots(1, 1)
-
-    wbds = get_contiguous_wbds()
-
-    other_bounds = [(b, "k") for b in wbds]
-
-    west, south, east, north = (
-        -127.441406,
-        24.207069,
-        -66.093750,
-        53.382373,
-    )
-    m = setup_map(
-        ax=ax, coords=[west, south, east, north], other_bound=other_bounds
-    )
+    fig, ax, m = setup_wbd_map()
 
     grand = gpd.read_file(
         (config.get_dir("spatial_data") / "grand_info").as_posix()
@@ -91,7 +78,58 @@ def plot_trimming_changes_map(noncon=False):
     plt.show()
 
 
+def plot_data_diff_map(results, year1, year2):
+    fig, ax, m = setup_wbd_map()
+    grand = gpd.read_file(config.get_dir("spatial_data") / "my_grand_info")
+    big_grand = gpd.read_file(config.get_file("grand_file"))
+    big_grand["GRAND_ID"] = big_grand["GRAND_ID"].astype(str)
+
+    yr1_data, yr1_meta = load_resopsus_data(year1)
+    yr2_data, yr2_meta = load_resopsus_data(year2)
+
+    grand = grand.set_index("GRAND_ID")
+
+    yr1_coords = grand.loc[
+        yr1_meta.index, ["LONG_DD", "LAT_DD"]
+    ].values.tolist()
+    yr2_coords = grand.loc[
+        yr2_meta.index, ["LONG_DD", "LAT_DD"]
+    ].values.tolist()
+
+    yr1_x, yr1_y = list(zip(*yr1_coords))
+    yr2_x, yr2_y = list(zip(*yr2_coords))
+
+    yr1_z = 4
+    yr2_z = 5
+    if len(yr1_x) < len(yr2_x):
+        yr1_z = 4
+        yr2_z = 5
+
+    m.scatter(
+        yr1_x,
+        yr1_y,
+        latlon=True,
+        label=f"Min Years={year1}",
+        marker="v",
+        zorder=yr1_z,
+    )
+    m.scatter(
+        yr2_x,
+        yr2_y,
+        latlon=True,
+        label=f"Min Years={year2}",
+        marker="v",
+        zorder=yr2_z,
+    )
+
+    ax.legend(loc="lower left")
+
+    plt.show()
+
+
 if __name__ == "__main__":
     sns.set_theme(context="talk", palette=Vivid_5.mpl_colors)
     # determine_huc2_trimming_changes(True)
     plot_trimming_changes_map(True)
+    # plot_data_diff_map(results, int(min_years), int(min_years) + 1)
+    # plot_data_diff_map(results, 3, 5)
